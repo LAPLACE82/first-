986419d9e1a7f4ad268b84b234f4aec0269ddaf7
cmy666 <1483238308@qq.com>
2022-04-16 16:08:48 +0800

    首次提交Cperson文件

---
+++ cperson.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<unistd.h>
+#include<unistd.h>
+using namespace std;
+class Cperson
+{
+	private:
+		Cperson(){}
+		~Cperson(){}
+	private:
+		static Cperson * ps;
+		static pthread_mutex_t lock;
+}
